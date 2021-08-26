5fcaf55bfc021f129b606918a69dcb7ef59b3373
Joseph Johnson <johnson_jt@outlook.com>
2021-08-25 21:36:21 -0400

    Initial commit NEO project

---
+++ extract.py
@@ -0,0 +1,64 @@
+"""Extract data on near-Earth objects and close approaches from CSV and JSON files.
+
+The `load_neos` function extracts NEO data from a CSV file, formatted as
+described in the project instructions, into a collection of `NearEarthObject`s.
+
+The `load_approaches` function extracts close approach data from a JSON file,
+formatted as described in the project instructions, into a collection of
+`CloseApproach` objects.
+
+The main module calls these functions with the arguments provided at the command
+line, and uses the resulting collections to build an `NEODatabase`.
+
+You'll edit this file in Task 2.
+"""
+import csv
+import json
+
+from models import NearEarthObject, CloseApproach
+from json_funct import Extract
+
+def load_neos(neo_csv_path='tests/test-neos-2020.csv'):
+    """Read near-Earth object information from a CSV file.
+
+    :param neo_csv_path: A path to a CSV file containing data about near-Earth objects.
+    :return: A collection of `NearEarthObject`s.
+    """
+    # TODO: Load NEO data from the given CSV file.
+    neos_dict = {}
+    neos = []
+    keys = ['pdes', 'name', 'diameter', 'pha']
+    with open(neo_csv_path, 'r') as neos_file:
+        reader = csv.DictReader(neos_file)
+        for row in reader:
+            designation = row['pdes']
+            name = row['name']
+            diameter = row['diameter']
+            if row['pha'] == 'Y':
+                hazardous = True
+            if row['pha'] == 'N' or row['pha'] == '':
+                hazardous = False
+            neo = NearEarthObject(designation,name,diameter,hazardous)
+            neos.append(neo)
+    return neos
+
+def load_approaches(cad_json_path='tests/test-cad-2020.json'):
+    """Read close approach data from a JSON file.
+
+    :param neo_csv_path: A path to a JSON file containing data about close approaches.
+    :return: A collection of `CloseApproach`es.
+    """
+    # TODO: Load close approach data from the given JSON file.
+    ca = []
+    with open(cad_json_path, 'r') as cad_file:
+        contents = json.load(cad_file)
+        #for desc, info in contents.items():
+          #  desc, info = contents['fields'], contents['data']
+         #   data += info
+        #fields += desc
+        for item in contents['data']:
+            ca.append(CloseApproach(item[0],time=item[3],distance=float(item[4]),velocity=float(item[7])))
+            
+    return ca
+
+load_approaches()
---
+++ json_funct.py
@@ -0,0 +1,3 @@
+def Extract(lst):
+    for i in range(12):
+        return [item[i] for item in lst]
