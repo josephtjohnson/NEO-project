73ee5c0966956f652c99c53f3505da0fefe0de2a
josephtjohnson <82980802+josephtjohnson@users.noreply.github.com>
2021-08-27 10:25:14 -0400

    removed unneeded comments

--- write.py
+++ write.py
@@ -26,8 +26,7 @@ def write_to_csv(results, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
-    with open('CloseApproach.csv', 'w') as outfile:
+    with open(filename,'w') as outfile:
         writer = csv.DictWriter(outfile,fieldnames=fieldnames)
         writer.writeheader()
         for row in results:
@@ -52,7 +51,7 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
+
     ca_list = []
     for row in results:
         ca_dict = {"datetime_utc": datetime_to_str(row.time),
@@ -60,11 +59,9 @@ def write_to_json(results, filename):
                    "velocity_km_s": row.velocity,
                    "neo": {
                        "designation": row.neo.designation,
-                       "name": row.neo.name,
+                       "name": row.neo.name if row.neo.name else '',
                        "diameter_km": row.neo.diameter,
                        "potentially_hazardous": row.neo.hazardous}}
-        
         ca_list.append(ca_dict)
-    with open('CloseApproach.json', 'w') as outfile:
-        json.dump(ca_list, outfile,)
-        
+    with open(filename, 'w') as outfile:
+        json.dump(ca_list, outfile, indent=4)
